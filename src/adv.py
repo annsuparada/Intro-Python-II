caa6770f8e0a2e4845b6bf4f467796585f9793e4
Ann <ann.suparada@gmail.com>
2020-12-11 16:29:39 -0600

    MVP day 1

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -37,15 +38,57 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# [X] Make a new player object that is currently in the 'outside' room.
 
 # Write a loop that:
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
+# * [X] Prints the current room name
+# * [X] Prints the current description (the textwrap module might be useful here).
+# * [X] Waits for user input and decides what to do.
 #
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
+# [x] If the user enters a cardinal direction, attempt to move to the room there.
+# [x] Print an error message if the movement isn't allowed.
 #
-# If the user enters "q", quit the game.
+# [x] If the user enters "q", quit the game.
+
+player1 = Player("Ann", room['outside'])
+
+
+def game():
+    print(f"You are at\n {player1.current_room}\n")
+
+    for x in room.values():
+        direction = input('Enter your direction n, s, e, w: --> ')
+
+        if direction == "n":
+            if x.n_to != None:
+                player1.current_room = x.n_to
+                print(player1.current_room)
+            else:
+                print('\n There is no room that way. Try again!\n')
+        elif direction == "s":
+            if x.s_to != None:
+                player1.current_room = x.s_to
+                print(player1.current_room)
+            else:
+                print('\n There is no room that way. Try again!\n')
+        elif direction == "w":
+            if x.w_to != None:
+                player1.current_room = x.w_to
+                print(player1.current_room)
+            else:
+                print('\n There is no room that way. Try again!\n')
+        elif direction == "e":
+            if x.e_to != None:
+                player1.current_room = x.e_to
+                print(player1.current_room)
+            else:
+                print('\n There is no room that way. Try again!\n')
+        elif direction == "q":
+            print("Bye :)")
+            break
+        else:
+            print(f"\nPlease enter the corect command or 'q' to Quite")
+
+
+game()
